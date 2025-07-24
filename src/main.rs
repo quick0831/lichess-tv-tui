55297428eae4e2da8d172bbc4c9465b6c406c2ee
Quick Chang <quick0831@gmail.com>
2025-07-25 03:38:20 +0800

    add json deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,65 @@
 use std::io::{BufRead, BufReader};
 
+use serde::Deserialize;
+
+#[allow(dead_code)]
+#[allow(clippy::large_enum_variant)]
+#[derive(Debug, Deserialize)]
+#[serde(tag = "t", content = "d")]
+enum TvData {
+    #[serde(rename = "featured")]
+    Featured(FeaturedData),
+    #[serde(rename = "fen")]
+    Fen(FenData),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct FeaturedData {
+    id: String,
+    orientation: Color,
+    players: [Player; 2],
+    fen: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Player {
+    color: Color,
+    user: User,
+    rating: i32,
+    seconds: i32,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct User {
+    name: String,
+    title: Option<String>,
+    flair: Option<String>,
+    #[serde(default)]
+    patron: bool,
+    id: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct FenData {
+    fen: String,
+    lm: String,
+    wc: i32,
+    bc: i32,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+enum Color {
+    #[serde(rename = "white")]
+    White,
+    #[serde(rename = "black")]
+    Black,
+}
+
 fn main() {
     let a = ureq::get("https://lichess.org/api/tv/feed")
         .call()
@@ -11,9 +71,10 @@ fn main() {
 
     for line in a.lines() {
         match line {
-            Ok(line) => {
-                println!("{}", line);
-            }
+            Ok(line) => match serde_json::from_str::<TvData>(&line) {
+                Ok(data) => println!("{:?}", data),
+                Err(err) => panic!("JSON parse failed: {err}"),
+            },
             Err(_) => todo!(),
         }
     }
