a9499a2d80e657432cf04d25bb87c804e51ff1df
Quick Chang <quick0831@gmail.com>
2025-07-27 01:13:49 +0800

    add tutorial template from ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::{
-    io::{BufRead, BufReader},
+    io::{self, BufRead, BufReader},
     str::FromStr,
 };
 
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
 use serde::{Deserialize, Deserializer, de::Visitor};
 use shakmaty::{Square, fen::Fen};
 
@@ -92,7 +102,15 @@ where
     deserializer.deserialize_str(StrVisitor)
 }
 
-fn main() {
+fn main() -> io::Result<()> {
+    std::thread::spawn(get_lichess_tv);
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn get_lichess_tv() {
     let a = ureq::get("https://lichess.org/api/tv/feed")
         .call()
         .unwrap()
@@ -104,10 +122,90 @@ fn main() {
     for line in a.lines() {
         match line {
             Ok(line) => match serde_json::from_str::<TvData>(&line) {
-                Ok(data) => println!("{:?}", data),
+                Ok(_data) => { /* println!("{:?}", data) */ }
                 Err(err) => panic!("JSON parse failed: {err}"),
             },
             Err(_) => todo!(),
         }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => Ok(()),
+        }
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> io::Result<()> {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Counter App Tutorial ".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
